03c0efcea6ef8bab215d41c9ab3f8279448bdb73
Elton Jacob <elton.jacob@ufl.edu>
2019-02-11 21:31:19 -0500

    second commit

--- client/js/controllers/listingController.js
+++ client/js/controllers/listingController.js
@@ -16,8 +16,8 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     };
 
     $scope.deleteListing = function(index) {
-      var indexOf = $scope.listings.indexOf(index);
-      $scope.listings.splice(indexOf,1);
+      //var indexOf = $scope.listings.indexOf(index);
+      $scope.listings.splice(index,1);
       Listings.delete(index).then(function(response) {
         }, function(error) {
         console.log('Could not delete:', error);
