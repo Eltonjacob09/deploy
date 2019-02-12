ceb9b254759d5ceababee3e1bf0f6401e3fc767e
Elton Jacob <elton.jacob@ufl.edu>
2019-02-11 21:06:47 -0500

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is typically ignored by git to protect sensitive information, such as your database's username and password
+
+module.exports = {
+  db: {
+    uri: 'mongodb://elton:Esj170150@ds011705.mlab.com:11705/ufdirect', //place the URI of your mongo database here.
+  }, 
+  port: 8080
+};
